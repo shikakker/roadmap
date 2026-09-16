6414b908213cfbd0f99a41e53345ea4dfba0ac02
Egor Denisov <designer.egor@ya.ru>
2026-09-16 12:18:44 -0400

    fix: clean roadmap list handler lint errors

--- pages/api/list.ts
+++ pages/api/list.ts
@@ -1,12 +1,12 @@
 import redis, { databaseName } from 'lib/redis'
 
-export default async (req, res) => {
+async function listFeatures(req, res) {
   try {
     const data = await redis.zrange(databaseName, 0, -1, { withScores: true })
 
-    let result = []
+    const result = []
     for (let i = 0; i < data.length - 1; i += 2) {
-      let item = data[i]
+      const item = data[i]
       item['score'] = data[i + 1]
       result.push(item)
     }
@@ -16,3 +16,5 @@ export default async (req, res) => {
     res.status(400).json({ error })
   }
 }
+
+export default listFeatures
